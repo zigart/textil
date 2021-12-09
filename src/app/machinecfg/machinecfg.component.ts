c7a9d5c1432e8471aeb46c07fb230a9527af4ea2
zigart <agustinzigart@gmail.com>
2021-12-09 19:58:47 -0300

    Se doto de funcionalidad al boton de reemplazo de informacion en maquinas

--- src/app/machinecfg/machinecfg.component.ts
+++ src/app/machinecfg/machinecfg.component.ts
@@ -10,11 +10,20 @@ export class MachinecfgComponent implements OnInit {
   public numberMachine:number;
   public date =  Date.now();
   public review: boolean;
+  public newDate:string;
+  public reviewDate:string;
+
+  private toModify:any;
 
   constructor(private routerAct:ActivatedRoute) { 
+    //initializations
+    this.review = false;  
+    this.newDate = ""; // two way data binding
+    this.reviewDate = "";
+    this.toModify;
+    //------------------------------------------------------------
     let routerActiv:any = this.routerAct.snapshot.paramMap.get('machine');
     this.numberMachine = routerActiv;
-    this.review = false;  
   }
 
   ngOnInit(): void {
@@ -22,13 +31,23 @@ export class MachinecfgComponent implements OnInit {
   }
 
 
-  editReview(){
-    let formReview = document.getElementsByClassName('separate');
-    if (this.review === false) {
-      this.review = true;
-    }else{
-      this.review = false
+  //this function find the html element when somebady click the button
+   editForm(e:any){
+    let paragraph = e.path[1].childNodes[0];
+    this.toModify = paragraph;
+    let form = document.getElementById('form');
+    if (form) {
+      form.style.display = "block";
     }
   }
-  editDivide(){}
+
+  //button for final edit 
+  edit(){
+    let form = document.getElementById('form');
+    if (this.toModify && form) {
+      this.toModify.innerText = "ultima revision:" + this.newDate;
+      form.style.display = "none";
+    }
+
+  }
 }
