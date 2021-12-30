934835b1fea5c7202fb0fea7d07b9b386a6cecca
zigart <agustinzigart@gmail.com>
2021-12-30 16:34:18 -0300

    Se funcionalizo el boton de agregar maquina respecto a la base de datos

--- src/app/job/configuration/layoutmachine/layoutmachine.component.ts
+++ src/app/job/configuration/layoutmachine/layoutmachine.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 import { machine } from 'src/app/models/machine.model';
+import { dataService } from 'src/app/services/data.service';
 import { MachineService } from 'src/app/services/machine/machine.service';
 
 @Component({
@@ -7,14 +10,17 @@ import { MachineService } from 'src/app/services/machine/machine.service';
   templateUrl: './layoutmachine.component.html',
   styleUrls: ['./layoutmachine.component.scss']
 })
-export class LayoutmachineComponent implements OnInit {
+export class LayoutmachineComponent implements OnInit, OnDestroy {
   public machines: Array<any>;
+  private getMachinesSubscription:Subscription = new Subscription();
+  private newSubscription: Subscription = new Subscription();
 
   @Output() machineNumber = new EventEmitter<machine>();
   @Output() displayLayoutMachine = new EventEmitter<boolean>();
 
   constructor(
-    private machineService:MachineService) { 
+    private machineService:MachineService,
+    private dataService: dataService) { 
       this.machines = [];
     }
 
@@ -22,23 +28,36 @@ export class LayoutmachineComponent implements OnInit {
     this.getMachines();
   }
 
+  ngOnDestroy(): void {
+      this.getMachinesSubscription.unsubscribe();
+  }
+
   getMachines(){
-    this.machineService.machineList.subscribe(
+   this.getMachinesSubscription = this.machineService.machineList.subscribe(
       (response) =>{
         this.machines = response;
         console.log(this.machines);
       },
       (error)=>{
         console.log(error);
-      }
-    )
+      });
   }
 
 
   addMachine() {
     let lastItem = this.machines[this.machines.length - 1];
-    // let newItem = lastItem + 1;
-    // this.machines.push(newItem);
+    let sum = lastItem.machineNumber + 1;
+    let newMachine = new machine(sum, true);
+    this.newSubscription = this.dataService.addMachine(newMachine)
+    .pipe(concatMap(machines => this.dataService.getMachines()))
+    .subscribe(
+      (response)=>{
+        this.machineService.machineList.next(response);
+      },
+      (error) =>{
+        console.log(error);
+      }
+    )
   }
 
   redirect(machine:machine){
